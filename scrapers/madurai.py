81d838cf0e300c862426d2b7a0f77ec41f554390
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2018-09-11 11:44:36 +0530

    script to scrape PDF files from Project Madurai

---
+++ scrapers/madurai.py
@@ -0,0 +1,28 @@
+import scrapesie as F
+import argparse
+import os
+
+from tqdm import tqdm
+
+BASE = 'http://www.projectmadurai.org'
+SEED = 'http://www.projectmadurai.org/pmworks.html'
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--out", default='cc/', 
+        help='[cc/] Folder to store downloaded content')
+args = parser.parse_args()
+
+
+def fetch_PDF_links():
+    soup = F.link2soup(SEED, 'html5lib')
+    return [ BASE + a.attrs['href'] for a in soup.find_all('a')
+            if 'href' in a.attrs and a.attrs['href'][-3:] == 'pdf' ]
+
+
+if __name__ == '__main__':
+    links = fetch_PDF_links()
+    for link in tqdm(links):
+        F.download_through_requests(
+                url= link, 
+                filename= os.path.join(args.out, link.split('/')[-1]), 
+                timeout= 15)
